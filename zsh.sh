68f7288eae10ce18084117df26a81c9eb3c52fdd
anki-code <anki-code>
2020-03-09 16:09:02 +0300

    LD_LIBRARY_PATH

--- zsh.sh
+++ zsh.sh
@@ -1,13 +1,12 @@
 CDIR="$(cd "$(dirname "$0")" && pwd)"
 cd $CDIR
 
-# Creating this recursive symlink placed here instead of creating it while building to avoid recursive reading while scp
-[ ! -d run ] && ln -sf . run
-./zsh -fc "typeset -p fpath" | sed "s,./run,$CDIR,g" > .zshenv
-
+export LD_LIBRARY_PATH=$CDIR:$LD_LIBRARY_PATH
 export ZDOTDIR=$CDIR
 export PATH=$CDIR:$PATH
 export ZSH_DISABLE_COMPFIX=true
-export LD_LIBRARY_PATH=$CDIR:$LD_LIBRARY_PATH
 
+# Creating this recursive symlink placed here instead of creating it while building to avoid recursive reading while scp
+[ ! -d run ] && ln -sf . run
+./zsh -fc "typeset -p fpath" | sed "s,./run,$CDIR,g" > .zshenv
 ./zsh "$@"
